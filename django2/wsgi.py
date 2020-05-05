1b29badc7884a68df5eb8151b64e5e97ce14e8e0
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-05-05 12:17:59 -0300

    Fix media files

--- django2/wsgi.py
+++ django2/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
+from dj_static import Cling, MediaCling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django2.settings')
 
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
